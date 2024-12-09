051573b7a6b648f472af081c5b6ad07d9e1f36a6
1HeNryChEn1 <1696613854@qq.com>
2024-12-09 19:47:43 +0800

    Create BP and ABP for player and enemies, including gobling with spear and slbingshot. Create Player Character and some InputActon.

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,25 @@
+// CopyRight HenryChen
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY (EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+};
